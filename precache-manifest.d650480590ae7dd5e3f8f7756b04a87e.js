be074d1e48e147a0ce272860fada71c8a1de3044
shrestharohit <rohitshr98@gmail.com>
2020-02-11 16:51:30 +0545

    deploy

---
+++ precache-manifest.d650480590ae7dd5e3f8f7756b04a87e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "368937d1a05d6a92d3e8",
+    "url": "/CanteenApp/css/chunk-vendors.c83f3dde.css"
+  },
+  {
+    "revision": "4f9a2d9f2f4251e2a408241ca877770b",
+    "url": "/CanteenApp/img/canteen.4f9a2d9f.jpg"
+  },
+  {
+    "revision": "2b4c5b5ef918dc0f828a873d6035f910",
+    "url": "/CanteenApp/index.html"
+  },
+  {
+    "revision": "4fe06957487e1f044168",
+    "url": "/CanteenApp/js/app.913184c9.js"
+  },
+  {
+    "revision": "368937d1a05d6a92d3e8",
+    "url": "/CanteenApp/js/chunk-vendors.e45b9064.js"
+  },
+  {
+    "revision": "011d1fcc0109cee777ad929d16478851",
+    "url": "/CanteenApp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/CanteenApp/robots.txt"
+  }
+]);
